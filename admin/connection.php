ea3f140007f9677a5c082a85b793923cbe8904cb
kamilshaikh <iamkamilshaikh@gmail.com>
2022-05-30 19:40:15 +0530

    fourth commit

--- admin/connection.php
+++ admin/connection.php
@@ -1,5 +1,8 @@
 <?php
-    $db = mysqli_connect("localhost","root","","library");  //mysqli_connect("serverName","userName","password","databaseName")
+    // $db = mysqli_connect("localhost","root","","library");  //mysqli_connect("serverName","userName","password","databaseName")
+    
+    $db = mysqli_connect("sql205.epizy.com","epiz_31847479","nIXA5N3txGBCU",
+    "epiz_31847479_maclibrary");  //mysqli_connect("serverName","userName","password","databaseName")
 
     // if(!$db){
     //     die("Connection failed:". mysqli_connect_error());
